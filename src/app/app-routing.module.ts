91bc6014c6d83311cc3c7194dc8efac60b8cdcc1
lorionj10 <armandoxd_f.o.b@hotmail.com>
2023-04-03 18:06:55 -0700

    Proyecto subido

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    // cuando alguien entral al path
+    path: 'auth',
+    // carga sus hijos   que viene del producto authmodule
+                        // y cuando se cargue en memoria
+                        // (al ser una promesa)     entonces el modulo que va a regresar es el AuthModule
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    //component: ErrorPageComponent
+    redirectTo: '404'
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
